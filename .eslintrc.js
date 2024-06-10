1bc4393a06914997ba8e0ca09cafde080afbd12d
Sébastien Raynaud <raynaud.sn@gmail.com>
2024-06-10 14:22:42 +0200

    add eslint prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ["expo", "prettier"],
+  plugins: ["prettier", "simple-import-sort", "autofix"],
+  rules: {
+    "prettier/prettier": "warn",
+    "simple-import-sort/imports": "warn",
+    "simple-import-sort/exports": "warn",
+    "autofix/no-unused-vars": "warn",
+  },
+};
