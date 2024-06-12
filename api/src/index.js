e87b64ea458b0a3d4981b0250e9c37c5891a6650
Julien Vankemmel <julienvankemmel@MBP-de-Julien.keyneo.lan>
2024-06-12 10:34:48 +0200

    [BACK] : add mongo db

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 const env = require('dotenv').config()
+const mongoose = require('mongoose')
 
-const app = express();
-app.use(cors({ origin: true }));
-app.use(express.json());
+const app = express()
+app.use(cors({ origin: true }))
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_CONNECTION).then(
+    console.log('connected to db')
+).catch((e) => {
+    console.error(e.message)
+})
 
 app.get('/', (req, res) => {
     res.send('Hello world')
