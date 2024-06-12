abe3a9e26a957c4379547740e7bc8fe8231f6c89
Sébastien Raynaud <raynaud.sn@gmail.com>
2024-06-12 18:51:45 +0200

    add table component

---
+++ mobile/components/Table.tsx
@@ -0,0 +1,94 @@
+import { Box, Text } from '@gluestack-ui/themed'
+import { StyleSheet } from 'react-native'
+
+export type TableHeader<T> = { label: string; key: keyof T }[]
+
+export type TableProps<T> = {
+  rows: T[]
+  header: { label: string; key: keyof T }[]
+}
+
+export function Table<T extends Record<string, any>>({
+  header,
+  rows,
+}: TableProps<T>) {
+  return (
+    <Box style={styles.root}>
+      <Box style={styles.head}>
+        {header.map((item, index) => {
+          const hasBorderRight = index < header.length - 1
+          return (
+            <Box
+              style={[
+                styles.cell,
+                styles.borderBottomStrong,
+                hasBorderRight && styles.borderRight,
+              ]}
+            >
+              <Text
+                key={typeof item.key === 'string' ? item.key : index}
+                style={styles.text}
+              >
+                {item.label}
+              </Text>
+            </Box>
+          )
+        })}
+      </Box>
+
+      {rows.map((row, rowIndex) => {
+        const hasBorderBottom = rowIndex < rows.length - 1
+        return (
+          <Box key={rowIndex} style={styles.row}>
+            {header.map((item, cellIndex) => {
+              const hasBorderRight = cellIndex < header.length - 1
+              return (
+                <Box
+                  style={[
+                    styles.cell,
+                    hasBorderBottom && styles.borderBottom,
+                    hasBorderRight && styles.borderRight,
+                  ]}
+                >
+                  <Text
+                    key={typeof item.key === 'string' ? item.key : cellIndex}
+                    style={styles.text}
+                  >
+                    {row[item.key]}
+                  </Text>
+                </Box>
+              )
+            })}
+          </Box>
+        )
+      })}
+    </Box>
+  )
+}
+
+const styles = StyleSheet.create({
+  root: {},
+  head: {
+    flexDirection: 'row',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  cell: {
+    flex: 1,
+    padding: 12,
+    borderColor: '#3C3C4399',
+  },
+  borderBottom: {
+    borderBottomWidth: 1,
+  },
+  borderBottomStrong: {
+    borderBottomWidth: 2,
+  },
+  borderRight: {
+    borderRightWidth: 1,
+  },
+  text: {
+    textAlign: 'center',
+  },
+})
