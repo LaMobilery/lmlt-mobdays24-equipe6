dcad2e3a86ee9ff44c61b774e42eb716fabee8cf
Claire Merlin <cmerlin@lamobilery.fr>
2024-06-12 17:08:07 +0200

    camera

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -7,6 +7,8 @@ import { useClientOnlyValue } from "@/components/useClientOnlyValue";
 import { useColorScheme } from "@/components/useColorScheme";
 import Colors from "@/constants/Colors";
 
+import { getCurrentDate } from "../utils/date";
+
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>["name"];
@@ -25,8 +27,7 @@ export default function TabLayout() {
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
-      }}
-    >
+      }}>
       <Tabs.Screen
         name="index"
         options={{
@@ -67,6 +68,24 @@ export default function TabLayout() {
         options={{
           title: "Setup",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          headerTitle: getCurrentDate(),
+          headerTitleStyle: {
+            textTransform: "uppercase",
+          },
+          headerRight: () => (
+            <Link href="/info" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="info-circle"
+                    size={25}
+                    color={Colors[colorScheme ?? "light"].text}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
         }}
       />
     </Tabs>
--- mobile/app/(tabs)/setup.tsx
+++ mobile/app/(tabs)/setup.tsx
@@ -1,6 +1,7 @@
+import { AddIcon, Button, ButtonIcon, ButtonText } from "@gluestack-ui/themed";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
-import { Button, Image, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 import { axiosInstance } from "@/api/client";
 
@@ -34,14 +35,14 @@ const sendImageToAI = async (base64: string) => {
     "https://api.openai.com/v1/chat/completions",
     payload,
   );
-  console.log(res);
+  console.log(res.data.choices[0]);
 };
 
 export default function SetupScreen() {
-  const [image, setImage] = useState<string>("");
+  const [, setImage] = useState<string>("");
   const [,] = useState([]);
 
-  const pickImage = async () => {
+  const pickImageCameraRoll = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       base64: true,
@@ -56,25 +57,45 @@ export default function SetupScreen() {
       }
     }
   };
+  const pickImageCamera = async () => {
+    const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+    if (permissionResult.granted === false) {
+      alert("You've refused to allow this appp to access your camera!");
+      return;
+    }
+    const result = await ImagePicker.launchCameraAsync();
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      const base64Image = `data:image/jpeg;base64,${result.assets[0].base64}`;
+      try {
+        await sendImageToAI(base64Image);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {/*<Button
+        title="Pick an image from camera roll"
+        onPress={pickImageCameraRoll}
+      />
+      <Button title="Pick an image from camera" onPress={pickImageCamera} />
       {image && (
         <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-      )}
+      )}*/}
+      <Button
+        size="md"
+        variant="solid"
+        action="primary"
+        isDisabled={false}
+        isFocusVisible={false}>
+        <ButtonText>Ajouter un légume </ButtonText>
+        <ButtonIcon as={AddIcon} />
+      </Button>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-});
+const styles = StyleSheet.create({});
